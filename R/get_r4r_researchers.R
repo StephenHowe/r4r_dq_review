8e4a6e954dc1c2baa4ac62b4b855e75413d9a9dc
StephenHowe <howetowork@gmail.com>
2025-02-22 12:48:10 -0500

    Outlined scripts

--- R/get_r4r_researchers.R
+++ R/get_r4r_researchers.R
@@ -1,19 +1,108 @@
 # Get R4R Researcher Data for Review
 # Stephen Howe
-# 21 February 2025
-# Version 1
+# 22 February 2025
+# Version 2
 
 # versions ----
 # v1 20250221 - initial script
+# v2 20250222 - added complete download of researcher docs in parquet
 
 # packages ----
 library(aws.s3)
 library(arrow)
 library(duckdb)
 library(dplyr)
+library(jsonlite)
+
+da_proj <- open_dataset("s3://com-copyright-agpipeline-sandds/data/internal/parquet/r4r/phyeng/graph/nodes/r4r_projection_authors/")
+duckdb_register_arrow(con, "da_proj_2", da_proj)
+
+# tet query
+foo_test <- dbGetQuery(
+  con,
+  "SELECT * FROM da_proj_2 WHERE persistent_identifier = 'r4r:01JMF0251C5SSK0K5DXP9W2HGG'"
+)
+
+
+
+# downlaod projection ----
+objects <- aws.s3::get_bucket_df(
+  bucket = "com-copyright-agpipeline-sandds",
+  prefix = "data/internal/parquet/r4r/phyeng/graph/nodes/r4r_projection_authors/"
+)
+
+getFiles <- function(x,y){
+  save_object(
+    object = x,
+    bucket = "com-copyright-agpipeline-sandds",
+    file = paste0("data/r4r_", y, ".parquet")
+  )
+}
+
+# test function
+# getFiles(
+#   "data/internal/parquet/r4r/phyeng/graph/nodes/r4r_projection_authors/part-00000-bc00116c-17a7-47e1-846b-1b6e171f616c-c000.snappy.parquet",
+#   "001"
+# )
+
+# run function en masse
+my_list <- objects$Key
+my_numbers <-rep(paste0("proj_", 1:nrow(objects)))
+
+mapply(getFiles, my_list, my_numbers)
+
+# duckdb connection to parquet docs
+con <- dbConnect(duckdb::duckdb())
+dbExecute(con, "CREATE TABLE da_proj AS SELECT * FROM read_parquet('data/*.parquet')")
+
 
 # data slices ----
+top_10 <- dbGetQuery(
+  con,
+  "SELECT *
+  FROM da_proj
+  ORDER BY occurrences DESC
+  LIMIT 10"
+)
+
+top_collab <- dbGetQuery(
+  con,
+  "SELECT *
+  FROM da_proj
+  ORDER BY colla"
+)
 
 
 
-# save for app ----
+
+
+
+### SCRATCH ####
+my_parquet <- list.files("data/",
+                         full.names = TRUE)
+data_dir <- "/Users/stephenhowe/Documents/code_active/r4r_dq_review/data"
+da_proj <-- open_dataset(
+  data_dir,
+  format = "parquet"
+)
+
+read_parquet(my_parquet[[1]])
+# da <- open_dataset("s3://com-copyright-agpipeline-sandds/data/internal/parquet/r4r/phyeng/graph/nodes/distinctauthor")
+# da_proj <- open_dataset("s3://com-copyright-agpipeline-sandds/data/internal/parquet/r4r/phyeng/graph/nodes/r4r_projection_authors/")
+# # au_docs <- open_dataset("s3://com-copyright-agpipeline-sandds/data/output/phyeng/json/graph/nodes/", format = "json")
+#
+#
+#
+#
+# # save for app ----
+#
+# getSaveJson <- function(x){
+#   da_proj |>
+#     filter(persistent_identifier == x) |>
+#     collect()
+# }
+#
+#
+#
+# # test and scratch
+# foo_test <- getSaveJson("r4r:01JMF0251C5SSK0K5DXP9W2HGG")
--- app/r4r_researcher_review/app.R
+++ app/r4r_researcher_review/app.R
@@ -1,51 +1,95 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# https://www.davidsolito.com/post/conditional-drop-down-in-shiny/
+# https://excelquick.com/r-shiny/selectinput-dependent-on-another-input/
 
+
+# From ChatGPT
 library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+# Define the available countries and their corresponding regions
+country_regions <- list(
+  "United States" = c("California", "Texas", "New York", "Florida", "Illinois"),
+  "Canada" = c("Ontario", "Quebec", "British Columbia", "Alberta", "Manitoba"),
+  "United Kingdom" = c("England", "Scotland", "Wales", "Northern Ireland"),
+  "Australia" = c("New South Wales", "Victoria", "Queensland", "Western Australia", "South Australia"),
+  "Germany" = c("Bavaria", "Berlin", "Hamburg", "Saxony", "Hesse")
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+# UI
+ui <- fluidPage(
+  titlePanel("Country and Region Selector"),
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("country", "Select a Country:",
+                  choices = names(country_regions), selected = "United States"),
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+      selectInput("region", "Select a Region:", choices = NULL)
+    ),
+
+    mainPanel(
+      h3("Selected Country and Region"),
+      textOutput("selection")
+    )
+  )
+)
+
+# Server
+server <- function(input, output, session) {
+
+  # Update the region dropdown based on the selected country
+  observeEvent(input$country, {
+    updateSelectInput(session, "region", choices = country_regions[[input$country]])
+  })
+
+  # Display selected values
+  output$selection <- renderText({
+    paste("Country:", input$country, "| Region:", input$region)
+  })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
+
+
+
+
+
+# ui <- fluidPage(
+#
+#     # Application title
+#     titlePanel("Old Faithful Geyser Data"),
+#
+#     # Sidebar with a slider input for number of bins
+#     sidebarLayout(
+#         sidebarPanel(
+#             sliderInput("bins",
+#                         "Number of bins:",
+#                         min = 1,
+#                         max = 50,
+#                         value = 30)
+#         ),
+#
+#         # Show a plot of the generated distribution
+#         mainPanel(
+#            plotOutput("distPlot")
+#         )
+#     )
+# )
+#
+# # Define server logic required to draw a histogram
+# server <- function(input, output) {
+#
+#     output$distPlot <- renderPlot({
+#         # generate bins based on input$bins from ui.R
+#         x    <- faithful[, 2]
+#         bins <- seq(min(x), max(x), length.out = input$bins + 1)
+#
+#         # draw the histogram with the specified number of bins
+#         hist(x, breaks = bins, col = 'darkgray', border = 'white',
+#              xlab = 'Waiting time to next eruption (in mins)',
+#              main = 'Histogram of waiting times')
+#     })
+# }
+
+# # Run the application
+# shinyApp(ui = ui, server = server)
