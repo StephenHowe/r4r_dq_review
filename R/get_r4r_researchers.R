9aeaf202a59d9ef0795d4330895d4ac93abb6ce5
StephenHowe <howetowork@gmail.com>
2025-02-21 20:58:02 -0500

    Project and directory setup

---
+++ R/get_r4r_researchers.R
@@ -0,0 +1,19 @@
+# Get R4R Researcher Data for Review
+# Stephen Howe
+# 21 February 2025
+# Version 1
+
+# versions ----
+# v1 20250221 - initial script
+
+# packages ----
+library(aws.s3)
+library(arrow)
+library(duckdb)
+library(dplyr)
+
+# data slices ----
+
+
+
+# save for app ----
